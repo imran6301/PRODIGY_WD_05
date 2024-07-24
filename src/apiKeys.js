d4cca425c6157868fa872aee95972c3ef415de0a
Imran <imranshaik6301@gmail.com>
2024-07-24 23:09:51 +0530

    first

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "81b2ce7a9aae4b4f200f9b69becda357",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
